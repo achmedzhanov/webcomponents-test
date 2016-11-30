c6fc820886049f57e9af81651143133240594174
Nail Achmedzhanov <achmedzhanov@gmail.com>
2016-11-30 15:33:55 +0400

    1) implemented  layout attributes
    2) used flex, moved element styles to css classes

--- app/scripts/tiles.js
+++ app/scripts/tiles.js
@@ -5,10 +5,12 @@ class XTiles extends HTMLElement {
   // [-] detect attribute changes
   // [-] layout
   // [+] css uppercase
-  // [-] center letters in tiles
-  // [-] square tiles
+  // [+] center letters in tiles
+  // [+] square tiles
   // [-] dynamic div size ans aspect ratio
   // [-] reuse dom elements
+  // [-] realtime attr changes
+
 
 
   constructor() {
@@ -17,11 +19,31 @@ class XTiles extends HTMLElement {
     let template = `
     <style>
       .container {
-        display: block;
+        display: flex;
         text-transform: uppercase;
       }
-      .container.column.direct {
-        display: block;
+      
+      .container div {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        text-align: center;
+        display: inline-block;
+      }
+      
+      .container.inline {
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        flex-wrap: wrap;
+      }      
+      
+      .container.inline.direct {
+        
+      }       
+      
+      .container.column {
+        flex-direction: column;
       }
       </style>
     `;
@@ -31,26 +53,58 @@ class XTiles extends HTMLElement {
     shadow.innerHTML = template;
 
     let container = document.createElement('div');
-    container.className = 'container';
+    this.container = container;
+
+    let layout = this.getAttribute('layout');
+    let order = this.getAttribute('order');
+
+    const LAYOUT_COLUMN = 'column';
+    const LAYOUT_INLINE = 'inline';
+
+    let layoutClass =  null;
+    switch(layout) {
+      case LAYOUT_COLUMN:
+        layoutClass = LAYOUT_COLUMN;
+        break;
+      case LAYOUT_INLINE:
+      default:
+        layoutClass = LAYOUT_INLINE;
+        break;
+    }
+
+    const ORDER_DIRECT = 'direct';
+    const ORDER_REVERSED = 'reversed';
+    const ORDER_RANDOMIZED = 'randomized';
+
+    let orderClass =  null;
+    switch(order) {
+      case ORDER_REVERSED:
+        orderClass = ORDER_REVERSED;
+        break;
+      case ORDER_RANDOMIZED:
+        orderClass = ORDER_RANDOMIZED;
+        break;
+      case ORDER_DIRECT:
+      default:
+        orderClass = ORDER_DIRECT;
+        break;
+    }
+
+    container.className = 'container ' + layoutClass + ' ' + orderClass;
     shadow.appendChild(container);
 
-    // TODO create container element
 
-    let text = this.getAttribute('data-text') || '';
+    let text = this.getAttribute('text') || '';
     let elements = _.filter([...(text)], (c) => c !== ' ')
       .map((c, i) => {
         let el = document.createElement("div");
         el.innerText = c;
-        el.style.width = '50px';
-        el.style.height = '50px';
-        el.style.lineHeight = '50px';
-        el.align = 'center';
-        el.style.display = 'inline-block';
         el.style.backgroundColor = rainbow(text.length, i);
         return el;
       });
 
-    _.each(elements, (el) => {container.appendChild(el)})
+    this.container.innerHTML = '';
+    _.each(elements, (el) => {this.container.appendChild(el)})
   }
 }
 
